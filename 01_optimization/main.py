164c4102dab1f176837d3ef2ef2d6d95928ae066
ELENI-NTAGKOULI <eleni.ntagkouli@students.iaac.net>
2025-06-15 23:39:12 +0200

    remove ignoring patches  process

--- 01_optimization/main.py
+++ 01_optimization/main.py
@@ -94,10 +94,7 @@ def insert_top_patches_to_supabase(df):
         utm_x = float(row["centroid_longitude"])
         utm_y = float(row["centroid_latitude"])
 
-            # Skip invalid UTM data
-        if utm_x > 180 or utm_y > 90:
-            print(f"⚠️ Skipping invalid UTM row: patch_id {row['patch_id']}")
-            continue
+        
         lon, lat = transformer.transform(utm_x, utm_y)
 
         rows.append({
