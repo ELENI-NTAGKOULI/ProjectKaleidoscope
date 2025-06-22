e9c72d0ac7905ad71a6bc3e79241d6a4229cdca2
ELENI-NTAGKOULI <eleni.ntagkouli@students.iaac.net>
2025-06-22 13:28:01 +0200

    correct launcher path again

--- 01_optimization/main1.py
+++ 01_optimization/main1.py
@@ -112,7 +112,7 @@ def main():
 
      # ➕ Launch tile server temporarily for tiling and upload
     os.environ["PROJECT_ID"] = project_id
-    subprocess.run(["python", "tile_launcher.py"], cwd="tile_server")
+    subprocess.run(["python", "tile_server/tile_launcher.py"], check=True)
 
 if __name__ == "__main__":
     main()
